7367cbd3ee7b11e588a2b1a41ff510bfd5a07833
Joseph DeWeese <inclinedtactics@icloud.com>
2024-04-11 22:46:29 -0500

    Initial Commit

---
+++ BodyTech/BodyTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyTechApp.swift
+//  BodyTech
+//
+//  Created by Joseph DeWeese on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BodyTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
